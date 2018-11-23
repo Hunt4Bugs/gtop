b6d4835934852d012c4272834c9752c5552256e7
Daniel Hunt <danielhunt@Daniels-MacBook-Pro.local>
2018-11-23 11:37:58 -0800

    internal stuff

---
+++ internal/app/gtop/tui.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	ui "github.com/gizak/termui"
+)
+
+func GetUI(header, arr []string) (*ui.List,*ui.List){
+	if err := ui.Init(); err != nil {
+		panic(err)
+	}
+	maxx := ui.TermWidth()
+	maxy := ui.TermHeight()
+
+	quarter := 4
+
+	h := ui.NewList()
+	h.Border = false
+	h.Width = maxx
+	h.Height = 4
+	h.Items = header
+	h.X = 0
+	h.Y = 0
+
+	table := ui.NewList()
+	table.Border = false
+	//table.Separator = false
+	table.Width = maxx
+	table.Height = maxy - quarter
+	table.Items = arr
+	table.X = 0
+	table.Y = quarter
+
+	// quits when q is pressed
+	ui.Handle("q", func(ui.Event) {
+		ui.StopLoop()
+	})
+	return h,table
+}
+
+func Render(h, t *ui.List){
+	ui.Render(h,t)
+}
+
+func Close(){
+	ui.Close()
+}
+
+func Loop(){
+	ui.Loop()
+}
